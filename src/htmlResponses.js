f08e5c5a9ed7dce7a0be8703e69ee933aff7c9d1
Nicholas Jones <nxj8268@g.rit.edu>
2020-02-03 22:32:41 -0500

    done with media assignment

---
+++ src/htmlResponses.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+// pull in the file system mod
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+  response.write(index);
+  response.end();
+};
+
+module.exports.getIndex = getIndex;
